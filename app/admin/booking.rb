85b9fade029e619fe1fd64b2a9d450494976c548
Arthur Littmann <arthur.littmann@gmail.com>
2016-12-21 17:15:08 +0000

    added tabs to admin dashboard

---
+++ app/admin/booking.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Booking do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :start_date, :end_date, :flat_id, :user_id, :price_cents, :status
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+
+end
--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,25 +3,20 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Bookings" do
+          ul do
+            Booking.last(10).map do |booking|
+              li link_to(booking.start_date, admin_booking_path(booking))
+            end
+          end
+        end
+      end
+    end
 
     #   column do
     #     panel "Info" do
